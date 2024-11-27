708077a3348e32926800b8fb1ff1207144e8cf83
JBOOGIE645 <julianfoleyiii13@gmail.com>
2024-11-27 16:20:18 -0500

    Commit

--- Sprint2PizzaProject/Sprint2PizzaProject/obj/Debug/net8.0-windows/Sprint2PizzaProject.AssemblyInfo.cs
+++ Sprint2PizzaProject/Sprint2PizzaProject/obj/Debug/net8.0-windows/Sprint2PizzaProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sprint2PizzaProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68178b7d0d6aee57b570293620cb420b1faab734")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7407ea6979c25cbffc548390ccf8331f67173dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sprint2PizzaProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sprint2PizzaProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
