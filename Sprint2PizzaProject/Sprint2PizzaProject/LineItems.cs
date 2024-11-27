dcf8124cee705faf4291af12ac8aee2bc5d2d6a3
Debora Sanchez <deborasanchez@Deboras-MacBook-Air.local>
2024-11-26 21:19:22 -0500

    Add constructors and G/S's for Address.cs and LineItems.cs

--- Sprint2PizzaProject/Sprint2PizzaProject/LineItems.cs
+++ Sprint2PizzaProject/Sprint2PizzaProject/LineItems.cs
@@ -17,10 +17,55 @@ namespace Sprint2PizzaProject
         private double price;
         private static int nextLineItemID = 10001;
 
-        //Make a constructor that sets inputted values to the objects values. For everything except lineItemID use this.(value) = (value)
+        //Make a constructor that sets inputed values to the objects values. For everything except lineItemID use this.(value) = (value)
 
+        public LineItems (int lineItemID, int orderID, int itemID1, int optionID1, int quantity, int itemID2, int optionID2, int itemID3, int optionID3, int itemID4, int optionID4, int itemID5, int optionID5,
+             int itemID6, int optionID6, int itemID7, int optionID7, string description, double price) {
+                
+                this.lineItemID = nextLineItemID;
+                this.orderID = orderID;
+                this.itemID1 = itemID1;
+                this.optionID1 = optionID1;
+                this.quantity = quantity;
+                this.itemID2 = itemID2;
+                this.optionID2 = optionID2;
+                this.itemID3 = itemID3;
+                this.optionID3 = optionID3;
+                this.itemID4 = itemID4;
+                this.optionID4 = optionID4;
+                this.itemID5 = itemID5;
+                this.optionID5 = optionID5;
+                this.itemID6 = itemID6;
+                this.optionID6 = optionID6;
+                this.itemID7 = itemID7;
+                this.optionID7 = optionID7;
+                this.description = description;
+                this.price = price;
+
+                nextLineItemID++;
+             }
 
         //Getters and setters for all values
+
+        public int LineItemID { get; set; }
+        public int OrderID { get; set; }
+        public int ItemID1 { get; set; }
+        public int OptionID1 { get; set; }
+        public int Quantity { get; set; }
+        public int ItemID2 { get; set; }
+        public int OptionID2 { get; set; }
+        public int ItemID3 { get; set; }
+        public int OptionID3 { get; set; }
+        public int ItemID4 { get; set; }
+        public int OptionID4 { get; set; }
+        public int ItemID5 { get; set; }
+        public int OptionID5 { get; set; }
+        public int ItemID6 { get; set; }
+        public int OptionID6 { get; set; }
+        public int ItemID7 { get; set; }
+        public int OptionID7 { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
 
     }
 }
